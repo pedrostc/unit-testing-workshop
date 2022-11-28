ac3a2514140a5b83727e5a4fd70cb9c4824b141a
Pedro Osternack Correa <pedro.oc@slalom.com>
2022-11-28 15:08:17 -0500

    Part 1 Solution

--- TestBootcamp.UnitTests/1-FizzBuzzTests.cs
+++ TestBootcamp.UnitTests/1-FizzBuzzTests.cs
@@ -2,29 +2,28 @@ using TestBootcamp._1_FizzBuzz;
 
 namespace TestBootcamp.UnitTests;
 
-/*
- * A test class is a collection of tests.
- * The convention for the test class name is {NameOfClassUnderTest}Tests.
- * In this example we're testing the `FizzBuzz` class.
- */
 [TestClass]
 public class FizzBuzzTests
 {
-    /*
-     * Each method inside of our test class marked with the `TestMethod` attribute is a test to be executed.
-     * The naming convention for tests is {NameOfMethodUnderTest}_{TestInput}_{ExpectedBehavior}
-     * In this example:
-     *  - we're testing the `Translate` method
-     *  - we're passing in a number that is not divisible by 3 or 5
-     *  - we expect it to return the number itself
-     */
-    [TestMethod]
-    public void Translate_NumberNotDivisibleBy3or5_TheNumberItself()
+    [DataTestMethod]
+    [DataRow(1, "1")]
+    [DataRow(2, "2")]
+    [DataRow(3, "Fizz")]
+    [DataRow(4, "4")]
+    [DataRow(5, "Buzz")]
+    [DataRow(15, "FizzBuzz")]
+    [DataRow(45, "FizzBuzz")]
+    [DataRow(60, "FizzBuzz")]
+    [DataRow(101, "101")]
+    public void Translate_Something_SomethingElse(int inputValue, string expectedResult)
     {
         //Arrange - Here we're setting up the necessary data or dependencies for our test
+        var fizzBuzz = new FizzBuzz();
         
         //Act - Here we'll perform the action that we want to test
-        
+        var result = fizzBuzz.Translate(inputValue);
+
         //Assert - Here we'll verify the results of the action performed by our code.
+        Assert.AreEqual(expectedResult, result);
     }
 }
